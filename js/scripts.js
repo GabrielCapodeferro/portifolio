56afed2d66bb9868f37d6eb8485c9b11c5eb447d
Gabriel Capodeferro <gabriel.capodeferro@aluno.ifsp.edu.br>
2025-03-21 19:22:53 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,32 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+function calcular(operacao) {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let num3 = parseFloat(document.getElementById('num3').value);
+    let resultado = 0;
 
-window.addEventListener('DOMContentLoaded', event => {
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('output').innerText = "Por favor, insira os dois primeiros números!";
+        return;
+    }
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
+    switch (operacao) {
+        case '+':
+            resultado = num1 + num2 + (isNaN(num3) ? 0 : num3);
+            break;
+        case '-':
+            resultado = num1 - num2 - (isNaN(num3) ? 0 : num3);
+            break;
+        case '*':
+            resultado = num1 * num2 * (isNaN(num3) ? 1 : num3);
+            break;
+        case '/':
+            if (num2 === 0 || (num3 === 0 && !isNaN(num3))) {
+                document.getElementById('output').innerText = "Não é possível dividir por zero!";
+                return;
             }
-        });
-    });
+            resultado = num1 / num2 / (isNaN(num3) ? 1 : num3);
+            break;
+    }
 
-});
+    document.getElementById('output').innerText = resultado;
+}
